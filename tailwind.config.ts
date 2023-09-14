ac4115ab5be41babf4a91097d67fe29b9cc9f10d
PedroAltamirano <pedroaal04@gmail.com>
2023-09-14 14:26:03 -0500

    fixes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,6 @@ import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    './index.html',
     './src/**/*.{js,ts,jsx,tsx,css,md,mdx,html,json,scss}',
   ],
   darkMode: 'class',
