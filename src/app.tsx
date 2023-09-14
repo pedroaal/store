01f9070048c4d98228e91e1d2fa0a72f3bb7f7f4
PedroAltamirano <pedroaal04@gmail.com>
2023-09-14 10:41:33 -0500

    init

--- src/app.tsx
+++ src/app.tsx
@@ -1,15 +1,8 @@
 import type { Component } from 'solid-js'
-import { QueryClient, QueryClientProvider } from '@tanstack/solid-query'
-
-import Tasks from './components/tasks'
-
-const queryClient = new QueryClient()
 
 const App: Component = () => {
   return (
-    <QueryClientProvider client={queryClient}>
-      <Tasks />
-    </QueryClientProvider>
+    <></>
   )
 }
 
--- src/config/task.ts
+++
@@ -1,9 +0,0 @@
-import { ITask } from '../types/task'
-
-export const DEFAULT_TASK: ITask = {
-  id: undefined,
-  title: '',
-  description: '',
-  status: 'PENDING',
-  createdAt: undefined
-}
